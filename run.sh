6760080dd0bcfccaec74b4c4d3f5b597fc9d54a5
algnot <thanawat.tongla@gmail.com>
2025-01-19 12:16:56 +0700

    fix: add port ro run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Waiting for MySQL to start..."
-until mysqladmin ping -h"$DATABASE_HOST" -u"$DATABASE_USERNAME" -p"$DATABASE_PASSWORD" --silent; do
+until mysqladmin ping -h"$DATABASE_HOST" -u"$DATABASE_USERNAME" -p"$DATABASE_PASSWORD" --port="$DATABASE_PORT" --silent; do
     sleep 2
 done
 echo "MySQL is up and running!"
