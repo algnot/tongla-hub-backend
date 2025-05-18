6bcba08f560f135f5fbc8e3ce78b95cd321c6c0f
algnot <thanawat.tongla@gmail.com>
2025-05-18 23:57:52 +0700

    feat:add openid

--- router/auth/get_openid_token.py
+++ router/auth/get_openid_token.py
@@ -1,21 +1,26 @@
 import requests
+import random
+import string
 from flask import Blueprint, request, jsonify
 from model.users import User
 from util.config import get_config
-from util.encryptor import encrypt
+from util.encryptor import encrypt, hash_password
 from util.request import handle_error, validate_request
 
 get_openid_token_app = Blueprint("get_openid_token", __name__)
 
 @get_openid_token_app.route("/get-openid-token", methods=["POST"])
-@validate_request(["token_endpoint", "user_info_endpoint", "redirect_uri", "code"])
+@validate_request(["code"])
 @handle_error
 def get_openid_token():
     payload = request.get_json()
 
     client_id = get_config("OPENID_CLIENT_ID", "")
     client_secret = get_config("OPENID_CLIENT_SECRET", "")
-    token_endpoint = f"{payload['token_endpoint']}?grant_type=authorization_code&client_id={client_id}&client_secret={client_secret}&redirect_uri={payload['redirect_uri']}&code={payload['code']}"
+    token_endpoint = get_config("OPENID_TOKEN_ENDPOINT", "")
+    user_info_endpoint = get_config("OPENID_USERINFO_ENDPOINT", "")
+    redirect_uri = get_config("OPENID_CLIENT_REDIRECT_URI", "")
+    token_endpoint = f"{token_endpoint}?grant_type=authorization_code&client_id={client_id}&client_secret={client_secret}&redirect_uri={redirect_uri}&code={payload['code']}"
 
     response = requests.request("POST", token_endpoint)
     if response.status_code != 200:
@@ -23,7 +28,7 @@ def get_openid_token():
 
     id_token = response.json()["id_token"]
 
-    user_info = requests.request("GET", payload["user_info_endpoint"], headers={
+    user_info = requests.request("GET", user_info_endpoint, headers={
         "Accept": "application/json",
         "Authorization": f"Bearer {id_token}"
     })
@@ -34,9 +39,11 @@ def get_openid_token():
     user = User().filter(filters=[("email", "=", encrypt(email))])
 
     if len(user) == 0:
+        random_password = ''.join(random.choices(string.ascii_letters + string.digits, k=20))
         user = User().create({
             "username": user_info.json()["username"],
             "email": email,
+            "hashed_password": hash_password(random_password)
         })
     else:
         user = user[0]
